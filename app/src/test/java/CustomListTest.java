70e5648b21019c87b86d216710236686feda10fa
Landyn Pugh <lpugh@ualberta.ca>
2022-03-09 14:56:59 -0700

    Added test for CustomList

---
+++ app/src/test/java/CustomListTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.assertEquals;
+
+import com.example.simpleparadox.listycity.City;
+import com.example.simpleparadox.listycity.CustomList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class CustomListTest {
+    private CustomList list;
+
+    @Before
+    public void createList(){
+        list = new CustomList(null, new ArrayList<City>());
+    }
+
+    @Test
+    public void addCityTest(){
+        int listSize = list.getCount();
+        list.addCity(new City("Halifax", "NS"));
+        assertEquals(list.getCount(), listSize+1);
+    }
+}
