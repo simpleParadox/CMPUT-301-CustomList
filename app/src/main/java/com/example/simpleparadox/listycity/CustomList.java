22f4dd14e96d5bb9dfedddc48c7a875828118b31
Mohamed Ali <55451465+dawood-ali@users.noreply.github.com>
2022-03-10 18:02:36 -0700

    hasCity implemented

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -70,10 +70,10 @@ public class CustomList extends ArrayAdapter<City> {
 
     public boolean hasCity(City city) {
 
-//        if (cities.contains(city)) {
-//
-//            return true;
-//        }
+        if (cities.contains(city)) {
+
+            return true;
+        }
         return false;
     }
 }
